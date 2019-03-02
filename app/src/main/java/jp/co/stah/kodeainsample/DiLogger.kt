e8e7c010e297c52570c94d90ca4046d3d9c35c82
yoshida0261 <yoshida261@gmail.com>
2019-03-03 02:11:53 +0900

    di logger 追加

---
+++ app/src/main/java/jp/co/stah/kodeainsample/DiLogger.kt
@@ -0,0 +1,10 @@
+package jp.co.stah.kodeainsample
+
+import android.util.Log
+
+
+class DiLogger : Logger {
+    override fun log(tag: String, message: String) {
+      //  Log.d(tag, message)
+    }
+}
